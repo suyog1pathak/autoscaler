1191d4c76b277c22b93a0a3bf3b23849facb3c59
suyog1pathak <suyog1pathak@gmail.com>
2024-06-22 00:20:42 +0530

    adding support for DownscaleAfterAttempts

--- pkg/autoscaler/autoscaler.go
+++ pkg/autoscaler/autoscaler.go
@@ -15,63 +15,69 @@ import (
 	"time"
 )
 
-var log *slog.Logger          // Logger instance for logging application events.
+var log *slog.Logger          // Logger instance for application events.
 var config *c.AppConfig       // AppConfig instance holding configuration settings.
-var downscaleReqCount int = 0 // Counter for tracking consecutive downscale requests.
+var downscaleReqCount int = 0 // Counter for consecutive downscale attempts.
 
 // Start initializes the auto-scaler and begins monitoring and adjusting replicas.
 func Start() {
-	// Initialize logger with JSON format for structured logging.
+	// Initialize logger with JSON format.
 	log = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	// Retrieve application configuration.
 	config = c.GetConfig()
 	// Adjust logger with configured log level.
 	log = slog.New(slog.NewJSONHandler(os.Stdout, util.LogLevelHandler(config.LogLevel)))
-	log.Info("starting autoscaler", "upstream", config.StatusEndpoint, "targetCpu", config.TargetCPU, "polling", config.PollInterval.Seconds(), "loglevel", config.LogLevel)
+	// Log startup information.
+	log.Info("Auto-scaler started", "upstream", config.StatusEndpoint, "targetCpu", config.TargetCPU, "pollingInterval", config.PollInterval.Seconds(), "logLevel", config.LogLevel)
 	// Start main loop for monitoring and adjusting replicas.
 	for {
-		// Fetch the current status
+		// Fetch current application status.
 		status, err := getStatus()
 		if err != nil {
-			log.Error("error fetching status", "error", err.Error())
+			log.Error("Failed to fetch status", "error", err.Error())
 			time.Sleep(config.PollInterval)
 			continue
 		}
 
-		// Calculate new number of replicas
-		newReplicas := calculateNewReplicas(status.CPU.HighPriority, status.Replicas)
+		// Calculate new number of replicas.
+		newReplicas := calculateNewReplicas(status.CPU.HighPriority, status.Replicas, config.DownscaleAfterAttempts)
 
 		if newReplicas != status.Replicas {
 			err = updateReplicas(newReplicas)
-			log.Debug("updated the number of replicas, waiting for cool down period")
+			// Log successful replica update.
+			log.Debug("Replica count updated, waiting for cooldown period")
 			time.Sleep(config.CoolDownPeriod)
 			if err != nil {
-				log.Error("error updating replicas", "error", err.Error())
+				// Log error updating replicas.
+				log.Error("Failed to update replicas", "error", err.Error())
 			}
 
 		}
-		log.Debug("recommendation is equal to the current replica, skipping replica update")
+		// Log skip replica update when recommendation matches current replicas.
+		log.Debug("No change in recommended replicas")
 
-		// Wait for the next poll interval
-		log.Debug("waiting for polling interval")
+		// Wait for the next polling interval.
+		log.Debug("Waiting for next polling interval")
 		time.Sleep(config.PollInterval)
 	}
 }
 
-// getStatus retrieves the current status of the application from the upstream service.
+// getStatus retrieves the current application status from the upstream service.
 func getStatus() (*model.StatusResponse, error) {
-	//log.Info("getting status for current pods and cpu")
+	// Make HTTP GET request to fetch status.
 	r, responseBody, err := rest.Client(http.MethodGet, config.StatusEndpoint, map[string]string{"Accept": "application/json"}, []byte(""), config.ApiTimeOut)
 	if err != nil {
 		return nil, err
 	}
+	// Handle non-OK status code from upstream service.
 	if r.StatusCode != http.StatusOK {
-		log.Warn("unable to check status from upstream service, will poll after interval", "interval", config.PollInterval.Seconds(), "response_code", r.StatusCode)
-		return nil, errors.New(fmt.Sprintf("unexpected status code %d", r.StatusCode))
+		log.Warn("Failed to fetch status from upstream service", "retryInterval", config.PollInterval.Seconds(), "statusCode", r.StatusCode)
+		return nil, errors.New(fmt.Sprintf("Unexpected status code %d", r.StatusCode))
 	}
 
 	var status model.StatusResponse
-	log.Info("current status", "response", util.ToJson(responseBody))
+	// Unmarshal JSON response into status model.
+	log.Info("Current status retrieved", "response", util.ToJson(responseBody))
 	err = json.Unmarshal(responseBody, &status)
 	if err != nil {
 		return nil, err
@@ -80,18 +86,20 @@ func getStatus() (*model.StatusResponse, error) {
 }
 
 // calculateNewReplicas computes the new number of replicas based on current CPU utilization.
-func calculateNewReplicas(currentCPU float64, currentReplicas int) int {
+func calculateNewReplicas(currentCPU float64, currentReplicas int, downscaleAfterAttempts int) int {
 	if currentCPU > config.TargetCPU {
-		log.Debug("current cpu utilization is more then threshold, adding more replicas")
+		// Log adding replicas due to high CPU utilization.
+		log.Debug("High CPU utilization detected, increasing replicas")
 		return int(math.Ceil(float64(currentReplicas) * (currentCPU / config.TargetCPU)))
 	} else {
-		//log.Debug("current cpu utilization is below threshold, reducing replicas")
-		if downscaleReqCount == 3 {
-			log.Debug("received downscaling recommendation")
+		// Handle downscaled recommendation based on downscale attempts.
+		if downscaleReqCount == downscaleAfterAttempts {
+			log.Debug("Received downscaled recommendation")
 			downscaleReqCount = 0
 			return int(math.Floor(float64(currentReplicas) * (currentCPU / config.TargetCPU)))
 		}
-		log.Debug("got downscale request as", "downscaleReqCount", downscaleReqCount)
+		// Log received downscale request.
+		log.Debug("Received downscale request", "downscaleReqCount", downscaleReqCount)
 		downscaleReqCount++
 		return currentReplicas
 	}
@@ -99,7 +107,8 @@ func calculateNewReplicas(currentCPU float64, currentReplicas int) int {
 
 // updateReplicas sends a request to update the number of replicas to the upstream service.
 func updateReplicas(newReplicas int) error {
-	log.Info("updating new replica count", "count", newReplicas)
+	// Log updating new replica count.
+	log.Info("Updating replica count", "count", newReplicas)
 	headers := map[string]string{
 		"Content-Type": "application/json",
 	}
@@ -111,12 +120,13 @@ func updateReplicas(newReplicas int) error {
 	r, _, err := rest.Client(http.MethodPut, config.ReplicasEndpoint, headers, requestBody, config.ApiTimeOut)
 
 	if err != nil {
-		log.Warn("unable to update new replica count on upstream service, will try after polling interval", "interval", config.PollInterval.Seconds(), "err", err.Error())
+		// Log error updating replicas.
+		log.Warn("Failed to update replica count on upstream service", "retryInterval", config.PollInterval.Seconds(), "error", err.Error())
 		return err
 	}
 
 	if r.StatusCode != http.StatusNoContent {
-		return errors.New(fmt.Sprintf("unexpected status code %d", r.StatusCode))
+		return errors.New(fmt.Sprintf("Unexpected status code %d", r.StatusCode))
 	}
 
 	return nil
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -10,13 +10,14 @@ import (
 
 // AppConfig holds configuration settings for the auto-scaler application.
 type AppConfig struct {
-	TargetCPU        float64       `mapstructure:"targetCPU"`        // TargetCPU is the target CPU utilization percentage (e.g., 0.80 for 80%).
-	StatusEndpoint   string        `mapstructure:"statusEndpoint"`   // StatusEndpoint is the URL endpoint for fetching current application status.
-	ReplicasEndpoint string        `mapstructure:"replicasEndpoint"` // ReplicasEndpoint is the URL endpoint for updating the number of application replicas.
-	PollInterval     time.Duration `mapstructure:"pollInterval"`     // PollInterval is the interval between consecutive status checks.
-	CoolDownPeriod   time.Duration `mapstructure:"coolDownPeriod"`   // CoolDownPeriod is the duration to wait after scaling replicas before making another scaling decision.
-	ApiTimeOut       time.Duration `mapstructure:"apiTimeOut"`       // ApiTimeOut is the timeout duration for API requests.
-	LogLevel         string        `mapstructure:"logLevel"`         // LogLevel sets the logging level for the application (e.g., "debug", "info", "warn").
+	TargetCPU              float64       `mapstructure:"targetCPU"`              // TargetCPU is the target CPU utilization percentage (e.g., 0.80 for 80%).
+	StatusEndpoint         string        `mapstructure:"statusEndpoint"`         // StatusEndpoint is the URL endpoint for fetching current application status.
+	ReplicasEndpoint       string        `mapstructure:"replicasEndpoint"`       // ReplicasEndpoint is the URL endpoint for updating the number of application replicas.
+	PollInterval           time.Duration `mapstructure:"pollInterval"`           // PollInterval is the interval between consecutive status checks.
+	CoolDownPeriod         time.Duration `mapstructure:"coolDownPeriod"`         // CoolDownPeriod is the duration to wait after scaling replicas before making another scaling decision.
+	ApiTimeOut             time.Duration `mapstructure:"apiTimeOut"`             // ApiTimeOut is the timeout duration for API requests.
+	LogLevel               string        `mapstructure:"logLevel"`               // LogLevel sets the logging level for the application (e.g., "debug", "info", "warn").
+	DownscaleAfterAttempts int           `mapstructure:"downscaleAfterAttempts"` // DownscaleAfterAttempts specifies the number of retry attempts after which the system initiates downscaling of resources.
 }
 
 var C *AppConfig
@@ -36,6 +37,7 @@ func createConfig() error {
 	viper.SetDefault("coolDownPeriod", 20*time.Second)
 	viper.SetDefault("logLevel", "DEBUG")
 	viper.SetDefault("apiTimeOut", 2*time.Second)
+	viper.SetDefault("downscaleAfterAttempts", 3)
 	// Unmarshal the configuration into the global AppConfig variable C.
 	if err := viper.Unmarshal(&C); err != nil {
 		log.Fatal("Error unmarshalling config:", err)
